47a351c35e6e247af264ceae143648a76886ab22
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-09-02 15:14:46 +0200

    Uppgift 2

---
+++ lab-1/uppgift-2.py
@@ -0,0 +1,25 @@
+def recept(antal):
+    ingredienser = [f"{round(3/4*antal)} ägg",
+                    f"{3/4*antal} dl strösocker",
+                    f"{3/4*antal} dl vetemjöl",
+                    f"{2/4*antal} tsk vaniljsocker",
+                    f"{2/4*antal} tsk bakpulver",
+                    f"{75/4*antal} g smör",
+                    f"{1/4*antal} dl vatten"]
+    
+    print(f"Ingredienser som behövs för en sockerkaka för {antal} personer:")
+    for ingrediens in ingredienser:
+        print(f"{ingrediens},")
+    
+def tid_blanda(antal):
+    return 10+antal
+
+def tid_grädda(antal):
+    return 30+(antal*3)
+
+def sockerkaka(antal):
+    recept(antal)
+    print(f"\nDet tar totalt {tid_blanda(antal) + tid_grädda(antal)} minuter att göra kakan\n")
+
+sockerkaka(4)
+sockerkaka(7)
