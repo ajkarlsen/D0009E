968b12e5d47d9770041b13eccd4fbbbe31edc191
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-09-05 12:06:10 +0200

    labb2 klar

--- lab-2/ekvation.py
+++ lab-2/ekvation.py
@@ -4,12 +4,6 @@ def derivative(f, x, h):
     ans = (1.0/(2*h))*(f(x+h)-f(x-h))
     return ans 
 
-#derivative(math.sin, math.pi, 0.0001)
-#derivative(math.cos, math.pi, 0.0001)
-#derivative(math.sin, math.pi/2, 0.0001)
-
-
-
 def solve(f, x0, h):
     while True:
         var = x0 - (f(x0)) / derivative(f, x0, h)
@@ -17,4 +11,12 @@ def solve(f, x0, h):
             return var
         x0 = var
 
-print(solve(math.sin, math.pi, 0.0001))
+def func1(x):
+    return x**2 - 1
+
+def func2(x):
+    return x - math.e**(-x) 
+
+print(solve(math.sin, 4, 0.001))
+print(solve(func1, 4, 0.0001))
+print(solve(func2, 3, 0.0001))
