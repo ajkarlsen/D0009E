d082a00329a4caedd8e9e7c911a30f8d040c3aa5
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-09-16 15:18:08 +0200

    Uppdaterad kommentar

--- lab-2/ekvation.py
+++ lab-2/ekvation.py
@@ -7,9 +7,9 @@ def derivative(f, x, h):
 def solve(f, x0, h): 
     while True:
         var = x0 - (f(x0)) / derivative(f, x0, h) #Kör x0 - funktionen genom dess derivata och sparar detta i en ny variabel (var)
-        if abs(var - x0) < h: #Kollar om beloppet minus den ursprungliga x0 är mindre än det tillåtna felet h
+        if abs(var - x0) < h: #Kollar om beloppet av var minus den ursprungliga x0 är mindre än det tillåtna felet h, om detta är fallet har vi kommit fram till ett nollställe
             return var #Ifall detta är fallet bryts loopen och funktionen returnerar nollstället (var)
-        x0 = var #Om var - x0 fortfarande är för stort så erstätts x0 med var. Detta så att funktionen kan prövas igen fast med ett uppdaterat x0
+        x0 = var #Om var - x0 fortfarande är för stort så erstätts x0 med var. Detta så att funktionen kan prövas igen fast med ett uppdaterat x0. Detta kommer till slut ta oss till ett nollställe
 
 def func1(x):
     return x**2 - 1
