d58dc624b65de44a43427b19d6ecce21ed6243c2
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-09-16 15:04:48 +0200

    Insert1 and Lookup1 Done

--- lab-3/ordlista.py
+++ lab-3/ordlista.py
@@ -1,8 +1,17 @@
-def insert():
-    pass
+def insert_2list():
+    word = input("Word to insert: ")
+    definition = input("Description of word: ")
+    list_words.append(word)
+    list_def.append(definition)
 
-def lookup():
-    pass
+def lookup_2list():
+    word = input("Word to lookup: ").lower()
+    if word in list_words:
+        print(f'{list_words[list_words.index(word)]}: {list_def[list_words.index(word)]}')
+    else: print("Word not in list")
+
+list_words = ["banan", "substantiv", "fotboll"]
+list_def = ["gul suspekt frukt", "namn på ting till exempel boll och ring", "sfär som man sparkar på"]
 
 def main():
     while True:
@@ -15,10 +24,13 @@ def main():
         if choice == "3":
             break
         elif choice == "1":
-            insert()
+            insert_2list()
         elif choice == "2":
-            lookup()
+            lookup_2list()
         else: 
             print("Invalid input")
 
+
+
 main()
+
