536e745a87b42cab4e3e588921baa037cc5e0f1d
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-09-23 11:04:44 +0200

    Ändrat så att inga globala variabler finns

--- lab-3/ordlista.py
+++ lab-3/ordlista.py
@@ -1,4 +1,4 @@
-def insert1(): #Function which allows user to input a word and a definition to be added to the dictionary
+def insert1(list_words, list_def): #Function which allows user to input a word and a definition to be added to the dictionary
     word = input("\nWord to insert: ")
     if word in list_words:
         print("Word already in list\n")
@@ -8,13 +8,13 @@ def insert1(): #Function which allows user to input a word and a definition to b
     list_words.append(word)
     list_def.append(description)
 
-def lookup1():
+def lookup1(list_words, list_def):
     word = input("\nWord to lookup: ")
     if word in list_words:
         print(f'{list_words[list_words.index(word)]}: {list_def[list_words.index(word)]}') #Finds index of given word and prints it out with its definition
     else: print("Word not in list") #Failsafe if word is not in dictionary
 
-def insert2():
+def insert2(list_tuple):
     word = input("\nWord to insert: ")
     for i in list_tuple:
         if i[0] == word:
@@ -23,7 +23,7 @@ def insert2():
     description = input("Description of word: ")
     list_tuple.append((word, description))
 
-def lookup2():
+def lookup2(list_tuple):
     inp = input("\nWord to lookup: ")
     for word, description in list_tuple:
         if word == inp:
@@ -31,7 +31,7 @@ def lookup2():
             break   
     else: print("Word not in list\n")
     
-def insert3():
+def insert3(word_dict):
     word = input("\nWord to insert: ")
     if word in word_dict:
         print("Word already in list")
@@ -39,22 +39,17 @@ def insert3():
     description = input("Description of word: ")
     word_dict.update({word:description})
 
-def lookup3():
+def lookup3(word_dict):
     word = input("\nWord to lookup: ")
     if word in word_dict:
         print(f"{word}: {word_dict[word]}")
     else: print("Word not in list")
     
-list_words = []
-list_def = []
-
-list_tuple = []
-
-word_dict = {}
-
-def main(): #Main function which outputs the user interface and handles inputs
+def main1(): #Main function which outputs the user interface and handles inputs
+    list_words = []
+    list_def = []
+    
     print("Menu for dictionary")
-
     while True:
         print("\n1. Insert")
         print("2. Lookup")
@@ -64,15 +59,50 @@ def main(): #Main function which outputs the user interface and handles inputs
         if choice == "3":
             break
         elif choice == "1":
-            insert1()
-            #insert2()
-            #insert3()
+            insert1(list_words, list_def)
         elif choice == "2":
-            lookup1()
-            #lookup2()
-            #lookup3()
+            lookup1(list_words, list_def)
         else: 
             print("Invalid input")
 
-main()
+def main2(): #Main function which outputs the user interface and handles inputs
+    list_tuple = []
+    
+    print("Menu for dictionary")
+    while True:
+        print("\n1. Insert")
+        print("2. Lookup")
+        print("3. Exit program\n")
+        choice = input("Choose alternative: ")
 
+        if choice == "3":
+            break
+        elif choice == "1":
+            insert2(list_tuple)
+        elif choice == "2":
+            lookup2(list_tuple)
+        else: 
+            print("Invalid input")
+            
+def main3(): #Main function which outputs the user interface and handles inputs
+    word_dict = {}
+    
+    print("Menu for dictionary")
+    while True:
+        print("\n1. Insert")
+        print("2. Lookup")
+        print("3. Exit program\n")
+        choice = input("Choose alternative: ")
+
+        if choice == "3":
+            break
+        elif choice == "1":
+            insert3(word_dict)
+        elif choice == "2":
+            lookup3(word_dict)
+        else: 
+            print("Invalid input")
+
+main1()
+main2()
+main3()
