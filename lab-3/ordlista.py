37fea7bba36b9708ec8ee21aba2db8904cd84c6d
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-09-24 10:37:21 +0200

    Labb 3 redovisad

--- lab-3/ordlista.py
+++ lab-3/ordlista.py
@@ -3,44 +3,51 @@ def insert1(list_words, list_def): #Function that allows user to add word to dic
     if word in list_words: #Checks if word already is there, if so returns and prints failsafe
         print("Word already in list")
         return
+    
     description = input("Description of word: ") 
-
     list_words.append(word) #Appends word and description to the 2 lists
     list_def.append(description)
 
 def lookup1(list_words, list_def): #Function which checks for the description of chosen word
     word = input("\nWord to lookup: ")
-    if word in list_words: #Checks if the asked for word exists in the dictionary, if not, prints failsafe
+    
+    if word in list_words:
         print(f'{list_words[list_words.index(word)]}: {list_def[list_words.index(word)]}') #Finds index of given word and prints it out with its definition
-    else: print("Word not in list") #Failsafe if word is not in dictionary
+    else: print("Word not in list") 
 
 def insert2(list_tuple): #Inserts word into dictionary with list of tuples, Takes in a list of tuples
     word = input("\nWord to insert: ")
+    
     for i in list_tuple: #Checks if the first element of any tuple is the word that wants to be added, if so prints failsafe and return
         if i[0] == word:
             print("Word already in list")
             return
+        
     description = input("Description of word: ") # If word not already in list it appends it as a tuple to the list
     list_tuple.append((word, description))
 
 def lookup2(list_tuple): #Inputs list of tuples, finds the word and description of the word that the user wants to check
     inp = input("\nWord to lookup: ")
+    
     for word, description in list_tuple: #Goes through the list and creates 2 variables for the 2 values
-        if word == inp: #Checks if its there
+        if word == inp:
             print(f"{word}: {description}")  
             break   
     else: print("Word not in list\n") #Failsafe if not there
     
 def insert3(word_dict): #Function which handles the user input of word and desc and then adds it to dictionary, takes a dictionary
     word = input("\nWord to insert: ")
+    
     if word in word_dict:
         print("Word already in list")
         return
+    
     description = input("Description of word: ")
     word_dict.update({word:description})
 
 def lookup3(word_dict): #Function which asks user for a word to check description for, takes a dictionary
     word = input("\nWord to lookup: ")
+    
     if word in word_dict:
         print(f"{word}: {word_dict[word]}")
     else: print("Word not in list")
