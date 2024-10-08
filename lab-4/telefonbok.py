abbe85a22649d1fa3243d5200d46bf8fb47d815c
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-10-08 20:03:17 +0200

    check_if_exist function added

--- lab-4/telefonbok.py
+++ lab-4/telefonbok.py
@@ -1,4 +1,7 @@
 def add_person(name, number, dict):
+    if check_if_exist(dict, name, number):
+        print("duplicate name or number")
+        return 
     dict.update({name: number})
 
 def lookup_person(name, dict):
@@ -12,13 +15,44 @@ def lookup_person(name, dict):
     else: return f"{name} not in list"
 
 def alias(name, newname, dict):
+    if check_if_exist(dict, newname):
+        print("duplicate name or number")
+        return 
+    
     if name in dict:
         dict[(name, newname)] = dict[name]
         del dict[name]
+        return
+    for key in dict:
+        if isinstance(key, tuple) and name in key:
+            newKey = key + (newname,)
+            dict[newKey] = dict[key]
+            del dict[key]
+            return
     else: print(f"{name} not in list")
     
-def change_num():
-    pass
+def change_num(name, number, dict):
+    if check_if_exist(dict, name, number):
+        print("duplicate name or number")
+        return 
+    else:
+        if name in dict:
+            return dict.update({name: number})
+        for key in dict:
+            if isinstance(key, tuple) and name in key:
+                return dict.update({key: number})
+        else: print(f"{name} not in list")
+
+def check_if_exist(dict, name="&", number="&"):
+    if number in dict.values():
+        return True
+    elif name in dict.keys():
+        return True
+    
+    for key in dict:
+        if isinstance(key, tuple) and name in key:
+            return True
+    else: return False
 
 def save():
     pass
@@ -37,7 +71,6 @@ def main():
         if user_inp[0] == "add":
             if len(user_inp) == 3:
                 add_person((user_inp[1]), user_inp[2], phone_book)
-                print(phone_book)
             else:
                 print("Incorrect input")
                 
@@ -52,7 +85,8 @@ def main():
             else: print("Incorrect input")
            
         elif user_inp[0] == "change":
-            change_num()
+            if len(user_inp) == 3:
+                change_num(user_inp[1], user_inp[2], phone_book)
             
         elif user_inp[0] == "save":
             save()
