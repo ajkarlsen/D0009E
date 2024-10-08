fe26135cdda25cb6c8ad3451a055123da7ef4770
Alex Karlsen <alexkarlsen@Alexs-MacBook-Air.local>
2024-10-08 18:15:35 +0200

    add person done

--- lab-4/telefonbok.py
+++ lab-4/telefonbok.py
@@ -1,5 +1,5 @@
-def add_person():
-    pass
+def add_person(name, number, dict):
+    dict.update({name: number})
 
 def lookup_person():
     pass
@@ -16,3 +16,39 @@ def save():
 def load():
     pass
 
+def main():
+    
+    phone_book = {}
+    
+    while True:
+        
+        user_inp = input("phonebook> ").split()
+        
+        if user_inp[0] == "add":
+            if len(user_inp) == 3:
+                add_person(user_inp[1], user_inp[2], phone_book)
+                print(phone_book)
+            else:
+                print("Incorrect input")
+                
+        elif user_inp[0] == "lookup":
+            lookup_person()
+            
+        elif user_inp[0] == "alias":
+            alias()
+            
+        elif user_inp[0] == "change":
+            change_num()
+            
+        elif user_inp[0] == "save":
+            save()
+            
+        elif user_inp[0] == "load":
+            load()
+            
+        elif user_inp[0] == "quit":
+            break
+        
+        else: print("Command doesn't exist")
+        
+main()
